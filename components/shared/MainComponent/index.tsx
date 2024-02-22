59d8e647ae7f42707ef23d432d1332eeeeacae50
Luis Barufi <luisbarufi@gmail.com>
2024-02-22 20:01:47 -0300

    adds the Footer

--- components/shared/MainComponent/index.tsx
+++ components/shared/MainComponent/index.tsx
@@ -1,12 +1,14 @@
-import Header from '../Header/StorefrontHeader'
+import Header from '../Header/StorefrontHeader';
+import Footer from '../Footer/StorefrontFooter';
 
 const MainComponent: React.FC = ({ children }) => {
   return (
     <div className='d-flex flex-column sticky-footer-wrapper'>
       <Header />
-        <div className='container'>
-          { children }
-        </div>
+      <div className='container flex-fill'>
+        { children }
+      </div>
+      <Footer />
     </div>
   )
 }
